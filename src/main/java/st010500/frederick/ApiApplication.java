681336d5a02f1b48696f9688e0b6062ef061d577
Unknown <mark.g.ya@yandex.ru>
2018-03-04 20:31:43 +0200

    Fixes for publishing to server.
    Fixes for publishing to server.

--- src/main/java/st010500/frederick/ApiApplication.java
+++ src/main/java/st010500/frederick/ApiApplication.java
@@ -8,16 +8,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import st010500.frederick.ChatServises.Chat;
 
-@SpringBootApplication
-public class ApiApplication extends SpringBootServletInitializer {
+//@SpringBootApplication
+public class ApiApplication /*extends SpringBootServletInitializer */{
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
+		//SpringApplication.run(ApiApplication.class, args);
 		Chat.startChat();
 	}
-
+/*
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(ApiApplication.class);
-	}
+	}*/
 }
